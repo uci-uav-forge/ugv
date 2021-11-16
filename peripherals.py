6b0b4d93be12a9c37495802d3ac73cf786f26dde
benjamin-miranda <benjaminmiranda.iv@gmail.com>
2021-11-16 02:52:45 -0800

    rename ultrasonicsensor.py to peripherals.py

--- peripherals.py
+++ peripherals.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
+#ultrasonic sensor
 class uss:
     def __init__(self, trigport, echoport) -> None:
         self.trig = trigport
@@ -24,6 +25,20 @@ class uss:
         x = (self.start - self.stop) * 17150
         print(x)
         return x
+
+class servo:
+    def __init__(self, pin) -> None:
+        self.pin = pin
+        GPIO.setup(self.pin, GPIO.OUT)
+        self.servo = GPIO.PWM(11,50)
+        self.servo.start(0)
+
+    def moveTo(self, angle):
+        # angle in degrees, duty cycle 2-12 (0-180)
+        self.servo.ChangeDutyCycle(2+angle/18)
+        time.sleep(0.5)
+        self.servo.ChangeDutyCycle(0)
+
 
 if __name__ == '__main__':
     test = uss(18,24)
